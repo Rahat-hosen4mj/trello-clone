fdd316fbd4ef2d9097d80d790c292fb3a193d026
Your Name <you@example.com>
2022-12-08 01:44:14 +0600

    npm i styled-componet and implemnet it

--- src/App.js
+++ src/App.js
@@ -1,61 +1,74 @@
+import { connect, useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import TrelloActionButton from "./component/TrelloActionButton";
+import TrelloList from "./component/TrelloList";
+import { DragDropContext } from "react-beautiful-dnd";
+import { sort } from "./action";
+import styled from "styled-components";
 
-import { connect, useDispatch, useSelector } from 'react-redux';
-import './App.css';
-import TrelloActionButton from './component/TrelloActionButton';
-import TrelloList from './component/TrelloList';
-import {DragDropContext} from 'react-beautiful-dnd'
-import { sort } from './action';
 
 
 function App() {
-  const lists = useSelector(state => state.lists);
-  const dispatch = useDispatch()
-  
-  const onDragEnd = (result) =>{
+  const lists = useSelector((state) => state.lists);
+  const dispatch = useDispatch();
+  const ListContainer = styled.div`
+    display: flex;
+    border: 1px solid red;
+    
+`
+
+  const onDragEnd = (result) => {
     // todo reorderding logic
     // console.log(result)
-    const {destination, source, draggableId} = result;
+    const { destination, source, draggableId } = result;
     // console.log(destination, source, droppableId)
-    if(!destination){
+    if (!destination) {
       return;
     }
 
-    dispatch(sort(
-      source.droppableId,
-      destination.droppableId,
-      source.index,
-      destination.index,
-      draggableId
-    ))
-  }
+    dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId
+      )
+    );
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-    <div>
-    <h2>trello Clone</h2>
-    <div style={styles.listContainer}>
+      <div>
+        <h2>trello Clone</h2>
       
-      {
-        lists.map(list => (<TrelloList listId={list.id} title={list.title} cards={list.cards} key={list.id} />))
-      }
-      <TrelloActionButton list />
-    </div>
-    </div>
+        <ListContainer>
+
+          {lists.map((list) => (
+            <TrelloList
+              listId={list.id}
+              title={list.title}
+              cards={list.cards}
+              key={list.id}
+            />
+          ))}
+          <TrelloActionButton list />
+       
+          </ListContainer>
+      </div>
     </DragDropContext>
   );
-};
-
-
-const styles = {
-  listContainer:{
-    display: 'flex',
-    flexDirection: "row"
-  }
 }
 
-const mapStateToProps = state =>({
-  lists: state.lists
+// const styles = {
+//   listContainer:{
+//     display: "flex",
+//     flexDirection: "row"
+//   }
+// }
+
+const mapStateToProps = (state) => ({
+  lists: state.lists,
 });
 
-
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps)(App);
--- src/component/TrelloList.js
+++ src/component/TrelloList.js
@@ -2,37 +2,50 @@ import React from "react";
 import TrelloActionButton from "./TrelloActionButton";
 import TrelloCard from "./TrelloCard";
 import { Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Container = styled.div`
+background-color: #dfe3e6;
+border-radius: 3px;
+width: 300px;
+height: 100%;
+padding: 8px;
+margin-right: 10px;
+`
 
 const TrelloList = ({ title, cards, listId }) => {
   //  console.log('list id', listId)
   return (
     <Droppable droppableId={String(listId)}>
       {(provider) => (
-        <div {...provider.droppableProps} ref={provider.innerRef} style={styles.container}>
+        // <ListContainer {...provider.droppableProps} ref={provider.innerRef}>
+          <Container {...provider.droppableProps} ref={provider.innerRef}>
           <h4>{title}</h4>
           {cards.map((card, index) => (
-            <TrelloCard  
-            key={card.id} 
-            text={card.text}
-            index={index} 
-            id={card.id} />
+            <TrelloCard
+              key={card.id}
+              text={card.text}
+              index={index}
+              id={card.id}
+            />
           ))}
           <TrelloActionButton listId={listId} />
           {provider.placeholder}
-        </div>
+        {/* </ListContainer> */}
+        </Container>
       )}
     </Droppable>
   );
 };
 
-const styles = {
-  container: {
-    backgroundColor: "#dfe3e6",
-    borderRadius: 3,
-    width: 300,
-    height: "100%",
-    padding: 8,
-    marginRight: 10,
-  },
-};
+// const styles = {
+//   container: {
+//     backgroundColor: "#dfe3e6",
+//     borderRadius: 3,
+//     width: 300,
+//     height: "100%",
+//     padding: 8,
+//     marginRight: 10,
+//   },
+// };
 export default TrelloList;
